d29fd672c558aa0df06b99f63dceed75c453cffb
thehatclub <tanner.smiley321@gmail.com>
2024-10-15 13:51:22 -0500

    ui design complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { Container, Stack, Text } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import UserGrid from "./components/UserGrid";
+
+function App() {
+  return (
+    <Stack minH={"100vh"}>
+      <Navbar />
+
+      <Container maxW={"1200px"} my={4}>
+        <Text
+          fontSize={{ base: "xl", md: "50" }}
+          fontWeight={"bold"}
+          letterSpacing={"2px"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          mb={8}
+        >
+          <Text
+            as={"span"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+          >
+            My Besties
+          </Text>
+          🚀
+        </Text>
+
+        <UserGrid />
+      </Container>
+    </Stack>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import {
+  Container,
+  Box,
+  Flex,
+  Text,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import CreateUserModal from "./CreateuserModal";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW={"900px"}>
+      <Box
+        px={4}
+        my={4}
+        borderRadius={5}
+        bg={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
+          {/* left side */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={3}
+            display={{ base: "none", sm: "flex" }}
+          >
+            <img src="/react.png" alt="react logo" width={50} height={50} />
+            <Text fontSize={"40px"}>+</Text>
+            <img src="/python.png" alt="python logo" width={50} height={40} />
+            <Text fontSize={"40px"}>=</Text>
+            <img src="/explode.png" alt="explode logo" width={45} height={45} />
+          </Flex>
+          {/* right side */}
+          <Flex gap={3} alignItems={"center"}>
+            <Text
+              fontSize={"lg"}
+              fontWeight={500}
+              display={{ base: "none", md: "block" }}
+            >
+              BFFship 🔥
+            </Text>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+            </Button>
+            <CreateUserModal />
+          </Flex>
+        </Flex>
+      </Box>
+    </Container>
+  );
+};
+
+export default Navbar;
